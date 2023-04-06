e63159ba6bae91037869530c31c27c627f6642a5
Arturo Perez <dash_arturo@hotmail.com>
2023-04-05 23:17:50 -0600

    Dinamismo en la ventana portafolio para que de acuerdo a la imgen que hizo click se muestre el json apartir del index de la imagen del index que se hizo click

--- js/informacion_portafolio.js
+++ js/informacion_portafolio.js
@@ -1,19 +1,42 @@
-window.onload = function() {fetch('js/informacion.json')
+window.onload = function() {
+   const productDetailsContainer = document.querySelector('.pr-det_container');
+   const index = new URLSearchParams(window.location.search).get('index');
+
+   fetch('js/informacion.json')
    .then(response => response.json())
-   .then(data => {
-      // Actualizar contenido HTML con datos del archivo JSON
-      document.getElementById('title').textContent = data.title;
+      .then(data => {
+         // Actualizar contenido HTML con datos del archivo JSON según el índice
+         const titleElement = productDetailsContainer.querySelector('#title');
+         titleElement.textContent = data[index].title;
 
-      const descriptionElement = document.getElementById('description');
-      data.description.forEach(paragraph => {
-         const p = document.createElement('p');
-         p.textContent = paragraph;
-         descriptionElement.appendChild(p);
-      });
+         const descriptionElement = productDetailsContainer.querySelector('#description');
+         descriptionElement.innerHTML = ""; // Limpiar contenido previo
 
-      document.getElementById('location').textContent = data.location;
-      document.getElementById('category').textContent = data.category;
-      document.getElementById('model').textContent = data.model;
-      document.getElementById('camera').textContent = data.camera;
-   }).catch(error => console.error('Error al cargar el archivo JSON', error));
+         data[index].description.forEach(paragraph => {
+            const p = document.createElement('p');
+            p.textContent = paragraph;
+            descriptionElement.appendChild(p);
+         });
+
+         const locationElement = productDetailsContainer.querySelector('#location');
+         locationElement.textContent = data[index].location;
+
+         const categoryElement = productDetailsContainer.querySelector('#category');
+         categoryElement.textContent = data[index].category;
+
+         const modelElement = productDetailsContainer.querySelector('#model');
+         modelElement.textContent = data[index].model;
+
+         const cameraElement = productDetailsContainer.querySelector('#camera');
+         cameraElement.textContent = data[index].camera;
+
+         // Mostrar el contenedor de detalles del producto
+         productDetailsContainer.style.display = 'block';
+      }).catch(error => console.error('Error al cargar el archivo JSON', error));
+
+   // Ocultar el contenedor de detalles del producto cuando se hace clic en el botón "Close Details"
+   const closeButton = productDetailsContainer.querySelector('.cpr_det');
+   closeButton.addEventListener('click', function() {
+      productDetailsContainer.style.display = 'none';
+   });
 }
